9cb92e88d3c8c5b12c67fe8c875f03cb77161bb8
larryhou <larryhou@foxmail.com>
2015-05-31 17:18:00 +0800

    Update geturl.js

--- geturl.js
+++ geturl.js
@@ -1,6 +1,8 @@
 $("tbody tr").map(function()
 {
 	var filename = $(this).data("name");
+	if (filename.indexOf("资源已被屏蔽") == 0) return ""
+		
 	var hash = $(this).data("hash");
 	var base64 = new TextUtil.base64();
 	var url = 'http://192.168.1.233/ftn_handler/' + filename + '?&&txf_fid=' + hash;
